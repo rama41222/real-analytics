cc236da5b234d9d3f0ded71e9ae2959938f964cd
rama41222 <dinushankanrg@gmail.com>
2020-07-27 19:29:46 +0530

    feat: raw data parser

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -3,8 +3,10 @@
  */
 const { response } = require('./response');
 const messages = require('./message.helper');
-
+const { parseAssetObject, parseUnitObject } = require('./parser');
 module.exports = {
   response,
   messages,
+  parseAssetObject,
+  parseUnitObject
 };
---
+++ src/lib/utils/parser.js
@@ -0,0 +1,23 @@
+/**
+ * Parses the raw data from csv files
+ *
+ * @param data
+ * @returns {Promise<*>}
+ */
+const parseUnitObject = async(data) => {
+  return {};
+};
+
+/**
+ * Parses the asset object
+ * @param data
+ * @returns {Promise<{}>}
+ */
+const parseAssetObject = async(data) => {
+  return {};
+};
+
+module.exports = {
+  parseUnitObject,
+  parseAssetObject
+};
